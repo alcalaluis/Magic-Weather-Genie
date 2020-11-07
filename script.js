21156e9acf86711145f6bad04871930381164d30
alcalaluis <alcalaluis@rocketmail.com>
2020-11-07 01:48:33 -0600

    pull weather JSON from API by city and render cities list

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+$("#find-weather").on("click", function(event){
 
-        var apiKey = "e411b7dc1e32be877fc4d36a692e8e12";
-        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?appid=e411b7dc1e32be877fc4d36a692e8e12&q=dallas";
+    event.preventDefault();
+    
+    var cityName = $("#city-input").val();
+    var apiKey = "e411b7dc1e32be877fc4d36a692e8e12";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?appid=e411b7dc1e32be877fc4d36a692e8e12&q=" + cityName + "&units=imperial";
 
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).then(function(response){
-            console.log(queryUrl);
-            console.log(response);         
-            });
-    
-    
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        $("#current-city").text(JSON.stringify(response));
+        console.log(queryUrl);
+        console.log(response);
+    });
+
+});
