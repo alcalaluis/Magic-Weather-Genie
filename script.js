29fd748b4de47f09ee2489f220d4ae06f9e64290
alcalaluis <alcalaluis@rocketmail.com>
2020-11-07 00:31:01 -0600

    api not console logging. scratched all and fixed

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-    function citySearch(){
-        var cityName = $("#city-input").val().trim()
+
         var apiKey = "e411b7dc1e32be877fc4d36a692e8e12";
-        var queryUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName +
-        "&appid=" + apiKey + "&units=imperial";
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?appid=e411b7dc1e32be877fc4d36a692e8e12&q=dallas";
 
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).then(function(response){
             console.log(queryUrl);
-            console.log(response);
-            $("#button-addon2").on("click", function(){
-                event.preventDefault();
-                citySearch();
+            console.log(response);         
             });
-        });
-    };
+    
+    
