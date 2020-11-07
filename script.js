f344501a1e130d8dbdd81b0d9b9713a2c922d23a
alcalaluis <alcalaluis@rocketmail.com>
2020-11-07 03:34:25 -0600

    i though i had it, but i don't at all. errors piling up

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
+var cities = [""];
+
+
 $("#find-weather").on("click", function (event) {
 
     event.preventDefault();
 
-    var cityName = $("#city-input").val();
-    var apiKey = "e411b7dc1e32be877fc4d36a692e8e12";
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?appid=e411b7dc1e32be877fc4d36a692e8e12&q=" + cityName + "&units=imperial";
+    var city = $("#city-input").val().trim().
+    cities.push(city);
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (response) {
-        $("#current-city").text(JSON.stringify(response));
-        console.log(queryUrl);
-        console.log(response);
-    });
 
+    function displayWeather() {
+        var cityName = $("#city-input").val();
+        var apiKey = "e411b7dc1e32be877fc4d36a692e8e12";
+        var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?appid=e411b7dc1e32be877fc4d36a692e8e12&q=" + cityName + "&units=imperial";
+
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+
+            var card = $("#current-city").text(cityName);
+
+            var temp = response.list.main.temp;
+
+            var tempItem = $("#temp").text("Temp:" + temp);
+
+            card.append(tempItem);
+
+
+        });
+    }
 });
 
-var cities = [""];
 
 function renderCities() {
+
     for (var i = 0; i < cities.length; i++) {
-        var city = $("<button>");
-        city.addClass("city")
-        city.attr("data-name", cities[i]);
-        city.text(cities[i]);
-        $("#city-list").append(city);
+        var cityEl = $("<button>");
+        cityEl.addClass("city-logs")
+        cityEl.attr("data-name", cities[i]);
+        cityEl.text(cities[i]);
+        $("#city-list").append(cityEl);
     }
 }
 
+
+$(document).on("click", ".city-list");
+
+renderCities();
